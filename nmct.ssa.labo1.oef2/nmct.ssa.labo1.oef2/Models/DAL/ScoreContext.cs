e33400937cd02a95186ef544b42815205902eee4
Thomas Toye <toye.thomas@gmail.com>
2015-02-10 17:19:33 +0100

    Add DB context

---
+++ nmct.ssa.labo1.oef2/nmct.ssa.labo1.oef2/Models/DAL/ScoreContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace nmct.ssa.labo1.oef2.Models.DAL
+{
+    public class ScoreContext
+    {
+        public DbSet<Competition> Competition { get; set; }
+        public DbSet<Score> Score { get; set; }
+        public DbSet<Team> Team { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
