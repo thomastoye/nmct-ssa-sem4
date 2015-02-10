de5b1bc039303a6140f5207e9b8e127c4ce690af
Thomas Toye <toye.thomas@gmail.com>
2015-02-10 17:21:10 +0100

    Enable migrations with the score context

---
+++ nmct.ssa.labo1.oef2/nmct.ssa.labo1.oef2/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace nmct.ssa.labo1.oef2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<nmct.ssa.labo1.oef2.Models.DAL.ScoreContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(nmct.ssa.labo1.oef2.Models.DAL.ScoreContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
