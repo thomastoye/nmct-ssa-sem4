b106d96a6b983c96db3d222d143cbfdd60e36a11
Thomas Toye <toye.thomas@gmail.com>
2015-02-10 16:45:56 +0100

    Add models

---
+++ nmct.ssa.labo1.oef2/nmct.ssa.labo1.oef2/Models/Competition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nmct.ssa.labo1.oef2.Models {
+    public class Competition {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Team> Teams { get; set; }
+        public List<Score> Scores { get; set; }
+        public Country Country { get; set; }
+
+    }
+}
---
+++ nmct.ssa.labo1.oef2/nmct.ssa.labo1.oef2/Models/Score.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nmct.ssa.labo1.oef2.Models {
+    public class Score {
+
+        public int Id { get; set; }
+        public Team TeamA { get; set; }
+        public int ScoreA { get; set; }
+        public Team TeamB { get; set; }
+        public int ScoreB { get; set; }
+        public int CompetitionId { get; set; }
+    }
+}
